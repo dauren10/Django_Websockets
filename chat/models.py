91658fc573067bc9280812f796826c2df941b1e3
OEM Configuration (temporary user) <dauren.shalabayev@gmail.com>
2023-05-01 23:24:24 +0600

    Chat model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from core import settings
+from django.contrib.auth.models import User
+class Chat(models.Model):
+    user =  models.ForeignKey(User, on_delete=models.CASCADE,)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Chat
+from .serializers import ChatSerializer
 
+class ChatListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
 
 def index(request):
     return render(request, "chat/index.html")
